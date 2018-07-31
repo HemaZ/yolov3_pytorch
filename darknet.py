b293dfc95ebf079a775696eea39be898e6131069
hema <egy.kid1@gmail.com>
2018-07-31 17:38:57 +0200

    update create_modules

--- darknet.py
+++ darknet.py
@@ -41,3 +41,40 @@ def parse_cfg(cfgfile):
 
 def create_modules(blocks):
     net_info=blocks[0]
+    prev_filters=3
+    module_list=nn.ModuleList()
+    output_filters=[]
+
+    for index,x in enumerate(blocks[1:]):
+        module=nn.Sequential()
+
+        if x["type"]=="convolutional":
+            kernel_size=int(x['size'])
+            stride=int(x['stride'])
+            pad=int(x['pad'])
+            activation=x['activation']
+            filters=int(x['filters'])
+            try:
+                batch_normalize=int(x['batch_normalize'])
+                bias=False
+            except:
+                batch_normalize=0
+                bias=True
+            if pad:
+                padding=(kernel_size-1)//2
+            else:
+                padding=0
+            conv=nn.Conv2d(prev_filters,filters,kernel_size,stride,padding,bias=bias)
+            module.add_module("conv_{0}".format(index), conv)
+
+            if batch_normalize:
+                bn=nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+            if activation=='leaky':
+                actv=nn.LeakyReLU(0.1,inplace=True)
+                module.add_module("leaky_{0}".format(index), actv)
+        elif x['type']=='upsample':
+            stride=int(x['stride'])
+            upsample=nn.Upsample(scale_factor=2,mode='bilinear')
+            module.add_module("upsample_{}".format(index), upsample)
+
